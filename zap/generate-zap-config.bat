94932781ba8a9a846b5c6418b2722b9e5a71b120
nhn0208 <nguyenhoainam.020803@gmail.com>
2025-07-07 11:31:59 +0700

    add static-config-part.yaml file

--- zap/generate-zap-config.bat
+++ zap/generate-zap-config.bat
@@ -1,56 +1,17 @@
-#!/bin/bash
+@echo off
 
-JWT=$(curl -s -X POST http://localhost:8080/api/auth/login \
-  -H "Content-Type: application/json" \
-  -d '{"username":"client","password":"client123"}' | jq -r '.token')
-echo "JWT token lấy được: $JWT"
+curl -s -X POST http://localhost:8080/api/auth/login ^
+  -H "Content-Type: application/json" ^
+  -d "{\"username\":\"client\", \"password\":\"client123\"}" > response.json
 
-cat <<EOF > zap/zap-automation.yaml
-env:
-  vars:
-    API_BASE: http://localhost:8080
-    JWT_TOKEN: ${JWT}
+for /f "tokens=*" %%i in ('powershell -Command "(Get-Content response.json | ConvertFrom-Json).token"') do set TOKEN=%%i
 
-  contexts:
-    - name: jwt-context
-      urls:
-        - \${API_BASE}
-      includePaths:
-        - '\${API_BASE}/api/users/.*'
-        - '\${API_BASE}/api/orders/.*'
-      authentication:
-        method: httpHeader
-        parameters:
-          headerName: Authorization
-          headerValue: 'Bearer \${JWT_TOKEN}'
-      sessionManagement:
-        method: httpAuthSession
-      users:
-        - name: jwt-user
-          credentials: {}
-          parameters: {}
+echo Token is: %TOKEN%
 
-jobs:
-  - type: spider
-    name: spider-users-orders
-    parameters:
-      context: jwt-context
-      maxDuration: 2
+echo parameters: > zap/zap-automation.yaml
+echo   - name: target >> zap/zap-automation.yaml
+echo     value: 'http://localhost:8080' >> zap/zap-automation.yaml
+echo   - name: JWT >> zap/zap-automation.yaml
+echo     value: '%TOKEN%' >> zap/zap-automation.yaml
 
-  - type: passiveScan-wait
-    name: passive-scan
-
-  - type: activeScan
-    name: active-users-orders
-    parameters:
-      context: jwt-context
-      policy: Default Policy
-      maxRuleDurationInMins: 2
-
-  - type: report
-    name: generate-html-report
-    parameters:
-      template: traditional-html
-      reportDir: zap-reports
-      reportFile: zap-report.html
-EOF
+type zap/static-config-part.yaml >> zap/zap-automation.yaml
