e29856629c40185dea6972ed0947ddd709cdaa18
nhn0208 <nguyenhoainam.020803@gmail.com>
2025-07-10 14:57:50 +0700

    fix file bola powershell

--- zap/test-BOLA-vul.ps1
+++ zap/test-BOLA-vul.ps1
@@ -3,10 +3,12 @@ Write-Host "[INFO] Starting ZAP with BOLA script..."
 $zapJar = "C:\\Xanh\\tttn\\ZAP\\ZAP_2.16.1_Crossplatform\\ZAP_2.16.1\\zap-2.16.1.jar"
 $scriptPath = "zap/scripts/TestBOLA.js"
 
-java -Xmx512m -Djava.awt.headless=true `
-    -jar $zapJar `
-    -cmd `
-    -script $scriptFile `
-    -scriptType httpsender `
-    -scriptEngine ECMAScript `
-    -config api.disablekey=true
+& java `
+  "-Xmx512m" `
+  "-Djava.awt.headless=true" `
+  "-jar" $zapJar `
+  "-cmd" `
+  "-script" $scriptFile `
+  "-scriptType" "httpsender" `
+  "-scriptEngine" "ECMAScript" `
+  "-config" "api.disablekey=true"
